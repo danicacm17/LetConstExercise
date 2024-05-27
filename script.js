0c7e0359d53b7f30d2b21b054c4d98a464b156fa
Danica <danicacm@live.com>
2024-05-27 18:15:34 -0400

    added completed exercise

---
+++ script.js
@@ -0,0 +1,19 @@
+/* var PI = 3.14;
+PI = 42; // stop me from doing this! */
+
+/* Write an ES2015 Version */
+
+const PI = 3.14;
+
+
+// What is the difference between var and let?
+//  -var can be reassigned or redeclared, let cannot
+
+// What is the difference between var and const?
+//  -const can never be changed or redeclared, var can
+
+// What is the difference between let and const?
+//  -let can be reassigned, const cannot
+
+// What is hoisting?
+//  -If var, you can access the variable before it is declared, but its value will be undefined. If let, they are hoisted but not initialized.
